7a969f6a0e5f76b3e90dd32b2016bbded938c649
entremotivator <104661532+entremotivator@users.noreply.github.com>
2024-08-07 00:30:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import streamlit as st
 # Base URL
 base_url = "https://dev-web.chattersocial.io/rooms/"
 
+# Instructions
+st.markdown("""
+### Instructions to use Chatter Live Desktop
+1. While in the Chatter room, click the "Share Room" link at the top.
+2. Paste your link in this app.
+3. Press "Enter" to generate a new link.
+4. Take the new link and send it to your Desktop to open.
+""")
+
 # Input URL
 input_url = st.text_input("Enter the URL with the unique identifier:", "https://www.chattersocial.io/rooms/db2b9be8-ea26-471c-8f60-06aa7a8bf7e1")
 
